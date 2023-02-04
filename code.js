0c4dcd0d6d209920f6c6142a2846f63a1df8e03e
Ckandyckainz <74159422+Ckandyckainz@users.noreply.github.com>
2023-02-04 12:18:24 -0800

    added modes
    no more double/triple click to edit text

--- code.js
+++ code.js
@@ -6,13 +6,19 @@ let mcan = document.getElementById("mcan");
 let mctx = mcan.getContext("2d");
 mcan.width = window.innerWidth;
 mcan.height = window.innerHeight;
+let toolbar = document.getElementById("toolbar");
+let eventNew = document.getElementById("eventnew");
+let modesDiv = document.getElementById("modes");
+let modeButtons = modesDiv.children;
 let mcw = mcan.width;
 let mch = mcan.height;
 let eventGUIsDiv = document.getElementById("eventguisdiv");
 let mousePos = {x: 0, y: 0};
+let selectedMode = {mode: "Move", index: 1};
+modeButtons[1].style.background = "gold";
 
 class Event{
-    constructor(x, y, isNew){
+    constructor(x, y){
         this.x = x;
         this.y = y;
         let GUI = document.createElement("p");
@@ -20,37 +26,22 @@ class Event{
         GUI.innerText = "event";
         GUI.style.left = ""+x+"px";
         GUI.style.top = ""+y+"px";
+        GUI.contentEditable = "false";
         eventGUIsDiv.appendChild(GUI);
         this.GUI = GUI;
-        this.GUI.contentEditable = "false";
-        this.dragging = isNew;
+        this.dragging = true;
         this.clickPos = {x: 0, y: 0};
-        if (isNew) {
-            events.push(this);
-        }
+        eventGUIsDiv.appendChild(GUI);
+        events.push(this);
         this.GUI.addEventListener("click", (event)=>{
-            this.clickPos = {x: event.offsetX, y: event.offsetY};
-            if (isNew) {
-                if (this.GUI.contentEditable == "false") {
-                    this.dragging = !this.dragging;
-                }
-            } else {
-                let evnt = new Event(mch*0.025, mch*0.875, true);
-                evnt.clickPos = this.clickPos;
+            if (selectedMode.mode == "Move") {
+                this.clickPos = {x: event.offsetX, y: event.offsetY};
+                this.dragging = !this.dragging;
             }
-        });
-        this.GUI.addEventListener("dblclick", ()=>{
-            if (!this.dragging) {
-                this.GUI.contentEditable = "true";
-            }
-        });
-        this.GUI.addEventListener("", ()=>{
-            console.log(0);
         });
     }
 }
 let events = [];
-let eventNew = new Event(mch*0.025, mch*0.875, false);
 
 function newCTButtonClicked(){
     homePage.style.display = "none";
@@ -59,10 +50,12 @@ function newCTButtonClicked(){
 }
 newCTButton.addEventListener("click", newCTButtonClicked);
 
-mcan.addEventListener("click", ()=>{
-    events.forEach((evnt)=>{
-        evnt.GUI.contentEditable = false;
-    });
+eventNew.addEventListener("click", (event)=>{
+    if (selectedMode.mode == "Move") {
+        let rect = eventNew.getBoundingClientRect();
+        let evnt = new Event(rect.x, rect.y);
+        evnt.clickPos = {x: event.offsetX, y: event.offsetY};
+    }
 });
 
 document.addEventListener("mousemove", (event)=>{
@@ -73,6 +66,29 @@ document.addEventListener("mousemove", (event)=>{
         }
     });
 });
+
+function eventsEditable(canEdit){
+    if (canEdit) {
+        events.forEach((evnt)=>{
+            evnt.GUI.contentEditable = "true";
+            evnt.GUI.style.resize = "both";
+        });
+    } else {
+        events.forEach((evnt)=>{
+            evnt.GUI.contentEditable = "false";
+            evnt.GUI.style.resize = "none";
+        });
+    }
+}
+
+for (let i=1; i<modeButtons.length; i++) {
+    modeButtons[i].addEventListener("click", ()=>{
+        modeButtons[selectedMode.index].style.background = "white";
+        modeButtons[i].style.background = "gold";
+        selectedMode = {mode: modeButtons[i].innerText, index: i};
+        eventsEditable(selectedMode.mode == "Edit");
+    });
+}
 
 function drawingLoop(){
 
